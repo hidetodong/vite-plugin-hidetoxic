263f4aea46e3c7400984ad0c8c8e0c47467e5d68
Hidetodong <121945480@qq.com>
2022-11-27 15:00:36 +0800

    feat: init

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-export const one = 1
-export const two = 2
+import consola from "consola";
+import type { Plugin } from "vite";
+
+export default function HidetoxicPlugin(): Plugin {
+  return {
+    apply: "serve",
+    name: "vite:hidetoxic",
+    enforce: "post",
+    configResolved() {
+      consola.success("HIDETOXIC - INJECTED");
+    },
+  };
+}
